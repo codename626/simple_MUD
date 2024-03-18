45fc4e5489f254e65d878446eeb53875acb5774c
Daemo <9426953+codename626@users.noreply.github.com>
2024-03-18 15:05:45 +1100

    added english translation into comments

--- Food.cpp
+++ Food.cpp
@@ -9,21 +9,21 @@ Food::~Food(){
 }
 
 void Food::showAtribute(){
-	std::cout << "== 物品表 ==" << std::endl;
-	std::cout << "||名字: " << name << std::endl;
-	std::cout << "||类别: " << type << std::endl;
-	std::cout << "||加血: " << addhp << std::endl;
-	std::cout << "||等级: " << level << std::endl;
-	std::cout << "||稀有度: " << rarity << std::endl;
+	std::cout << "== 物品表 ==" << std::endl; //item list
+	std::cout << "||名字: " << name << std::endl; //name
+	std::cout << "||类别: " << type << std::endl; //category
+	std::cout << "||加血: " << addhp << std::endl; //add hp
+	std::cout << "||等级: " << level << std::endl; //level
+	std::cout << "||稀有度: " << rarity << std::endl; //rarity
     cout << description << endl;
     cout << "-----------------" << endl;
 }
 
 void Food::interact(){
-    cout << "其名为" << name << ", 是:" << type << endl;
+    cout << "其名为" << name << ", 是:" << type << endl; //"Its name is" "is:" 
     cout << description << endl;
-    cout << "请输入想做的动作:\n";
-    std::cout << "-1: 退出 0: 查看"<< std::endl;
+    cout << "请输入想做的动作:\n"; //Please enter the action you want to do:
+    std::cout << "-1: 退出 0: 查看"<< std::endl; //"-1: Exit 0: View"
     cout << "--------------------\n";
     int cho;
     while(std::cin >> cho){
